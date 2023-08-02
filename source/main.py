434a6201910bbfd19bd4e9e82b266dbcd1aec9fd
Honkou <filiphancke@gmail.com>
2023-08-02 15:46:36 +0200

    Add pre-commit

--- source/main.py
+++ source/main.py
@@ -2,9 +2,9 @@ import networkx as nx
 from pyvis.network import Network
 
 G = nx.Graph()
-G.add_nodes_from([1,3,5,7,9])
-G.add_edge(1,3)
-G.add_edge(5,9)
+G.add_nodes_from([1, 3, 5, 7, 9])
+G.add_edge(1, 3)
+G.add_edge(5, 9)
 
 print(G.number_of_edges())
 print(G.number_of_nodes())
