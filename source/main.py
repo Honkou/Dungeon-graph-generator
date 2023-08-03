205f8c6da9f6f03646441b293190267f38b9dec2
Honkou <filiphancke@gmail.com>
2023-08-03 09:37:57 +0200

    Replace proof of concept with actual logic

--- source/main.py
+++ source/main.py
@@ -1,15 +1,33 @@
 """Main file to run the program."""
+import random
 
 import networkx as nx
 from pyvis.network import Network
 
-G = nx.Graph()
-G.add_nodes_from([1, 3, 5, 7, 9])
-G.add_edge(1, 3)
-G.add_edge(5, 9)
 
-print(G.number_of_edges())
-print(G.number_of_nodes())
+def generate_graph(points: tuple) -> nx.Graph:
+    """Create a graph based on the points input."""
+    graph = nx.Graph()
+    for point in points:
+        graph.add_node(point, label=str(point))
+        if graph.number_of_nodes() > 1:
+            random_node = random.choice(find_proper_nodes(graph, point))  # noqa: S311
+            graph.add_edge(point, random_node)
+    return graph
+
+
+def find_proper_nodes(graph: nx.Graph, excluded: int) -> list:
+    """Return all nodes that match certain conditions."""
+    proper_nodes = []
+    for node in graph.nodes:
+        if node == excluded:
+            continue
+        proper_nodes.append(node)  # do: Add proper logic
+    return proper_nodes
+
+
+test_points = (1, 2, 3, 4, 5, 6)
+G = generate_graph(test_points)
 
 visnet = Network()
 visnet.from_nx(G)
