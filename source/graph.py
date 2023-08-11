8033448f52169aa7f6448a52d8be16b237232bae
Honkou <filiphancke@gmail.com>
2023-08-11 17:53:45 +0200

    Refactor graph drawing logic into ABC + nxGraph

---
+++ source/graph.py
@@ -0,0 +1,64 @@
+"""File for graph ABC and its implementations."""
+
+import random
+from abc import ABC, abstractmethod
+from collections.abc import Iterable
+
+import networkx as nx
+
+
+class Graph(ABC):
+
+    """Class for creating and analyzing graphs."""
+
+    @abstractmethod
+    def generate_graph(self, points: Iterable[int]) -> None:
+        """Based on the given points, create nodes with edges."""
+
+    @abstractmethod
+    def add_legal_edge(self, new_point: int) -> None:
+        """Create a connection between a freshly created node and an old one."""
+
+    @abstractmethod
+    def find_proper_nodes(self, excluded: int) -> list:
+        """Return all nodes that match certain conditions."""
+
+
+class NxGraph(Graph):
+
+    """Implementation of NetworkX library's graph."""
+
+    def __init__(self) -> None:
+        """Initialize the class and assign a graph object."""
+        self.graph = nx.Graph()
+        self._EDGE_TRESHOLD = 4
+
+    def generate_graph(self, points: Iterable[int]) -> None:
+        """Based on the points given, create nodes with edges."""
+        for point in points:
+            max_edge = random.randint(2, self._EDGE_TRESHOLD)
+            self.graph.add_node(point, label=str(point), max_edges=max_edge)
+
+            if self.graph.number_of_nodes() > 1:
+                self.add_legal_edge(point)
+
+    def add_legal_edge(self, new_point: int) -> None:
+        """Create a connection between a freshly created node and an old one."""
+        random_node = random.choice(self.find_proper_nodes(new_point))
+        if random_node == 0:
+            return
+        self.graph.add_edge(new_point, random_node)
+
+    def find_proper_nodes(self, excluded: int) -> list:
+        """Return all nodes that match certain conditions."""
+        proper_nodes = []
+        for node in self.graph.nodes:
+            if (
+                node == excluded
+                or self.graph.degree[node] >= nx.get_node_attributes(self.graph, "max_edges")[node]
+            ):
+                continue
+            proper_nodes.append(node)  # do: Add proper logic
+        if proper_nodes == []:
+            return [0]
+        return proper_nodes
--- source/main.py
+++ source/main.py
@@ -1,34 +1,11 @@
 """Main file to run the program."""
-import random
-
-import networkx as nx
+from graph import NxGraph
 from pyvis.network import Network
 
-
-def generate_graph(points: tuple) -> nx.Graph:
-    """Create a graph based on the points input."""
-    graph = nx.Graph()
-    for point in points:
-        graph.add_node(point, label=str(point))
-        if graph.number_of_nodes() > 1:
-            random_node = random.choice(find_proper_nodes(graph, point))  # noqa: S311
-            graph.add_edge(point, random_node)
-    return graph
-
-
-def find_proper_nodes(graph: nx.Graph, excluded: int) -> list:
-    """Return all nodes that match certain conditions."""
-    proper_nodes = []
-    for node in graph.nodes:
-        if node == excluded:
-            continue
-        proper_nodes.append(node)  # do: Add proper logic
-    return proper_nodes
-
-
 test_points = (1, 2, 3, 4, 5, 6)
-G = generate_graph(test_points)
+G = NxGraph()
+G.generate_graph(test_points)
 
 visnet = Network()
-visnet.from_nx(G)
+visnet.from_nx(G.graph)
 visnet.show("proof.html", notebook=False)
