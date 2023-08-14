b0b9fcdf1661c336887688da9143dc6634f80b29
Honkou <filiphancke@gmail.com>
2023-08-14 23:48:30 +0200

    Add more test points and improve graph's physics

--- source/main.py
+++ source/main.py
@@ -2,10 +2,25 @@
 from graph import NxGraph
 from pyvis.network import Network
 
-test_points = (1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+test_points = range(1, 21)
 G = NxGraph()
 G.generate_graph(test_points)
 
+physics_options = """const options = {
+  "physics": {
+    "barnesHut": {
+      "gravitationalConstant": -1450,
+      "centralGravity": 0.15,
+      "springLength": 155,
+      "springConstant": 0.015,
+      "damping": 0.45
+    },
+    "minVelocity": 0.75
+  }
+}"""
+
 visnet = Network()
 visnet.from_nx(G.graph)
+visnet.toggle_physics = True
+visnet.set_options(physics_options)
 visnet.show("proof.html", notebook=False)
